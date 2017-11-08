634ecc958efdcc0929aef9ee2a7fd347d78f8b79
Lesik <Lesik@users.noreply.github.com>
2017-11-08 12:22:04 +0100

    Styleguide

--- main.cpp
+++ main.cpp
@@ -19,11 +19,11 @@ int main()
 
     int k = 0;
     int l = 0;
-    int* A2 = new int[30*30];
-    for (int i = 0; i < 30*30; i++) {
+    int* A2 = new int[30 * 30];
+    for (int i = 0; i < 30 * 30; i++) {
         if (l % 30 == 0 && l != 0) {
             k = k + 1;
-            l = 0; 
+            l = 0;
             cout << "\n";
         }
         A2[i] = A1[k][l];
@@ -40,7 +40,7 @@ int main()
         cout << "\n 0. Exit\n 1. Change Cell\n 2. Reprint\n";
         cin >> auswahl;
 
-        switch(auswahl){
+        switch(auswahl) {
 
             case 0:
                 end = 1;
@@ -53,7 +53,7 @@ int main()
                 };
                 break;
             case 2:
-                for (int i = 0; i < 30*30; i++) {
+                for (int i = 0; i < 30 * 30; i++) {
                     if (l % 30 == 0 && l != 0) {
                         k = k + 1;
                         l = 0;
