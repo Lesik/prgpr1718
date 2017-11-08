7048b758c341229d9b840df151c7ee13ad3474d5
Lesik <Lesik@users.noreply.github.com>
2017-11-08 12:20:41 +0100

    Add text spacing

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-int main() 
-{   
+int main()
+{
     int A1[30][30];
-    
+
     for (int i = 0; i < 30; i++) {
         for (int j = 0; j < 30; j++) {
             A1[i][j] = rand() % 10;
@@ -14,9 +14,9 @@ int main()
         }
         cout << "\n";
     }
-    
+
     cout << "\n";
-    
+
     int k = 0;
     int l = 0;
     int* A2 = new int[30*30];
@@ -30,33 +30,33 @@ int main()
         std::cout << A2[i] << " ";
         l = l + 1;
     }
-    
+
     int end = 0;
     int auswahl;
-    
-    do {  
+
+    do {
         int k = 0;
         int l = 0;
-        cout << "\n0.Exit\n1.Change Cell\n2.Reprint\n";
+        cout << "\n 0. Exit\n 1. Change Cell\n 2. Reprint\n";
         cin >> auswahl;
 
         switch(auswahl){
 
-            case 0:  
+            case 0:
                 end = 1;
                 break;
-            case 1:  
+            case 1:
                 for (int i = 0; i < 30; i++) {
                     for (int j = 0; j < 30; j++) {
                         A1[i][j] = rand() % 10;
                     };
                 };
                 break;
-            case 2:  
+            case 2:
                 for (int i = 0; i < 30*30; i++) {
                     if (l % 30 == 0 && l != 0) {
                         k = k + 1;
-                        l = 0; 
+                        l = 0;
                         cout << "\n";
                     }
                     A2[i] = A1[k][l];
