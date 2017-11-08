688228f5106c730c55e42e4dee80f4993b1327c6
Kelviiiin <33090236+Kelviiiin@users.noreply.github.com>
2017-11-08 13:02:17 +0100

    Add files via upload

---
+++ milestone1aufgabe2/CAbase.h
@@ -0,0 +1,32 @@
+#ifndef CABASE_H
+#define CABASE_H
+
+class CAbase
+{
+private:
+    int Nx, Ny;                 // Menmbervariable
+    int* A1[30*30], A2[30*30];
+
+public:
+    CAbase(int x, int y); // Konstruktor
+    ~CAbase(); // Destruktor
+
+// Setter
+    void setGroese(int x, int y);
+    void setZelle(int x, int y);
+
+// Getter
+    int getNx();
+    int getNy();
+    bool getZelle(int x, int y);
+
+
+// 'Spiel des Lebens' - Regeln / Methoden
+    int nachbar(int x, int y);
+    void regel(int x, int y);
+
+    void Print();
+    void Envolve();
+};
+
+#endif // CABASE_H
