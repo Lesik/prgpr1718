7cea12f23a4fd5c327d9e459c6ef334651fc8ba9
Fatih Acar <fatih.acar@outlook.de>
2017-11-16 15:41:50 +0100

    falsche eingaben abgefangen

--- milestone1aufgabe2/main.cpp
+++ milestone1aufgabe2/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 // Startmenü
 void Start(bool end) {
     CAbase base(30, 30);
-    base.print();
 
     do {
+        base.print();
         const char *functions[] = {"Exit", "Evolve", "Set alive cell", "Reprint", "Resize"};
 
         for (int i = 0; i < 5; i++)
@@ -19,10 +19,28 @@ void Start(bool end) {
         cin >> zahl;
         switch (zahl) {
             case 0 : end = 1; break;
-            case 1 : base.evolve(); base.print(); break;
-            case 2 : cout << "Feld angeben: " << endl; cin >> x >> y; base.setCell(x - 1, y - 1, 1); break;
+            case 1 : base.evolve(); break;
+            case 2 :
+                cout << "Feld angeben, Reihe dann Spalte(1 bis n): " << endl;
+                cin >> x >> y;
+                if (x < base.getNx() && y < base.getNy()) {
+                    base.setCell(x - 1, y - 1, 1);
+                }
+                else {
+                    cout << "Falsche Eingabe!" << endl;
+                }
+                break;
             case 3 : base.print(); break;
-            case 4 : cout << "Größe angeben: " << endl; cin >> x >> y; base.setSize(x, y); break;
+            case 4 :
+                cout << "Groeße angeben(min. 3): " << endl;
+                cin >> x >> y;
+                if (x < 3 || y < 3) {
+                    cout << "Falsche Eingabe!" << endl;
+                }
+                else {
+                    base.setSize(x, y);
+                }
+                break;
         }
     } while (end == false);
 }
