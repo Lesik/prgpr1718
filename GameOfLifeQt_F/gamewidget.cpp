8d5a531167a07eb67f3522d2c5e801b558b39c99
Fatih <fatih.acar@outlook.de>
2017-11-18 00:12:14 +0100

    Komplett, muss nur noch kommentiert werden

---
+++ GameOfLifeQt_F/gamewidget.cpp
@@ -0,0 +1,134 @@
+#include "gamewidget.h"
+#include <QTimer>
+#include <QMouseEvent>
+#include <QRectF>
+#include <QPainter>
+#include <QString>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+using namespace std;
+
+GameWidget::GameWidget(QWidget *parent) :
+    QWidget(parent),
+    timer(new QTimer(this))
+{
+    connect(timer, SIGNAL(timeout()), this, SLOT(newGeneration()));
+}
+
+void GameWidget::setTimerIntervall(int t) {
+    timer->setInterval(t);
+}
+
+void GameWidget::setUniverseSize(int size) {
+    universeSize = size;
+    ca.setSize(size, size);
+    update();
+}
+
+void GameWidget::startGame() {
+    timer->start();
+}
+
+void GameWidget::stopGame() {
+    timer->stop();
+}
+
+void GameWidget::clear() {
+    setUniverseSize(universeSize);
+}
+
+void GameWidget::newGeneration() {
+    ca.evolve();
+    update();
+}
+
+void GameWidget::mousePressEvent(QMouseEvent *event) {
+    double cellGeometry = 580.00/universeSize;
+    int xPosition = event->x()/cellGeometry;
+    int yPosition = event->y()/cellGeometry;
+    if (ca.getCell(xPosition, yPosition) == 0) {
+        ca.setCell(xPosition, yPosition, 1);
+    }
+    else ca.setCell(xPosition, yPosition, 0);
+    update();
+}
+
+void GameWidget::mouseMoveEvent(QMouseEvent *event) {
+    double cellGeometry = 580.00/universeSize;
+    int xPosition = event->x()/cellGeometry;
+    int yPosition = event->y()/cellGeometry;
+    if (ca.getCell(xPosition, yPosition) == 0) {
+        ca.setCell(xPosition, yPosition, 1);
+    }
+    update();
+}
+
+void GameWidget::paintEvent(QPaintEvent *)
+{
+    paintGrid();
+    paintUniverse();
+}
+
+void GameWidget::paintGrid()
+{
+    QPainter painter(this);
+    QRect rec(0, 0, 580.00, 580.00);
+    double cellGeometry = 580.00 / universeSize;
+    for(double i = 0; i <= 580.00; i += cellGeometry) {
+        painter.drawLine(i, 0, i, 580.00);
+        painter.drawLine(0, i, 580.00, i);
+    }
+    painter.drawRect(rec);
+}
+
+void GameWidget::paintUniverse() {
+    QPainter painter(this);
+    double cellGeometry = 580.00 / universeSize;
+    for(int j = 0; j < universeSize; j++) {
+        for(int i = 0; i < universeSize; i++) {
+            if(ca.getCell(i, j) == 1) {
+                QRectF rec(i * cellGeometry, j * cellGeometry, cellGeometry, cellGeometry);
+                painter.fillRect(rec, Qt::black);
+            }
+        }
+    }
+}
+
+void GameWidget::saveToFile() {
+    QString fileName = QFileDialog::getSaveFileName(this,
+            tr("Save Current Universe"), "",
+            tr("Textfile (*.txt);;All Files (*)"));
+
+    QFile myFile(fileName);
+    if (myFile.open(QIODevice::ReadWrite | QIODevice::Text)) {
+            QTextStream stream(&myFile);
+            stream << universeSize << endl;
+            for (int j = 0; j < universeSize; j++) {
+                for (int i = 0; i < universeSize; i++) {
+                    stream << ca.getCell(i, j) << endl;
+                }
+            }
+    }
+    myFile.close();
+}
+
+void GameWidget::loadFromFile() {
+    QString fileName = QFileDialog::getOpenFileName(this,
+            tr("Load Saved Universe"), "",
+            tr("Textfile (*.txt);;All Files (*)"));
+    QFile myFile(fileName);
+    if (myFile.open(QIODevice::ReadWrite | QIODevice::Text)) {
+        QTextStream in(&myFile);
+        QString line = in.readLine();
+        setUniverseSize(line.toInt());
+        int i = 0;
+        while(!in.atEnd()) {
+            QString line = in.readLine();
+            ca.getCurrentWorld()[i] = line.toInt();
+            i++;
+        }
+    }
+
+    myFile.close();
+}
---
+++ GameOfLifeQt_F/main.cpp
@@ -0,0 +1,11 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    w.game->setUniverseSize(3);
+    return a.exec();
+}
