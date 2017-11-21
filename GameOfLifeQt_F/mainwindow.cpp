879158bf6aa922760dc6daef49d58c22b26e49ae
Fatih <fatih.acar@outlook.de>
2017-11-21 18:14:37 +0100

    Update mainwindow.cpp

--- GameOfLifeQt_F/mainwindow.cpp
+++ GameOfLifeQt_F/mainwindow.cpp
@@ -1,5 +1,5 @@
 /*
-    Fatih (), Javi (), Lesik (6081100), Kelvin Tsang (5428328)
+    Fatih (6251581), Javi (), Lesik (6081100), Kelvin Tsang (5428328)
 
     Milestone 1
 
@@ -16,42 +16,13 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->gridLayout->addWidget(game);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() {delete ui;}
 
-void MainWindow::on_Button_start_clicked()
-{
-    game->startGame();
-}
-
-void MainWindow::on_Button_stop_clicked()
-{
-    game->stopGame();
-}
-
-void MainWindow::on_Button_clear_clicked()
-{
-    game->clear();
-}
-
-void MainWindow::on_SpinBox_universe_size_valueChanged(int arg1)
-{
-    game->setUniverseSize(arg1);
-}
-
-void MainWindow::on_SpinBox_generation_intervall_valueChanged(int arg1)
-{
-    game->setTimerIntervall(arg1);
-}
-
-void MainWindow::on_Button_save_clicked()
-{
-    game->saveToFile();
-}
-
-void MainWindow::on_Button_load_clicked()
-{
-    game->loadFromFile();
-}
+// Widgets im mainwindow.ui werden zu Funktionen in gamewidget zugewiesen
+void MainWindow::on_Button_start_clicked() {game->startGame();}
+void MainWindow::on_Button_stop_clicked() {game->stopGame();}
+void MainWindow::on_Button_clear_clicked() {game->clear();}
+void MainWindow::on_SpinBox_universe_size_valueChanged(int arg1) {game->setUniverseSize(arg1);}
+void MainWindow::on_SpinBox_generation_intervall_valueChanged(int arg1) {game->setTimerIntervall(arg1);}
+void MainWindow::on_Button_save_clicked() {game->saveToFile();}
+void MainWindow::on_Button_load_clicked() {game->loadFromFile();}
